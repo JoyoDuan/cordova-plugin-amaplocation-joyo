90ae4662541a6c43e018425ae4518e0baa8440eb
JoyoDuan <JoyoDuan@163.com>
2019-04-16 18:14:27 +0800

    Update amap-location.js

--- www/amap-location.js
+++ www/amap-location.js
@@ -1,5 +1,6 @@
 var exec = require('cordova/exec');
 
+// 这里的命令AMapLocationPlugin与plugin.xml无关，用abc都可以，module.exports用于require('')
 function AMapLocationPlugin(){}
 
 AMapLocationPlugin.prototype.getLocation = function(successCallback, errorCallback) {
